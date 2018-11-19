ca7afeb8c8a7c26518d32ac0287ef56a524cd4c0
Rody Davis <rdavis@unifycrm.com>
2018-11-19 11:18:14 -0500

    Commenting

--- test/login_test.dart
+++ test/login_test.dart
@@ -1,42 +1,42 @@
-import 'package:MyUnifyMobile/keys.dart';
-import 'package:flutter_driver/flutter_driver.dart';
-import 'package:test/test.dart';
+// import 'package:MyUnifyMobile/keys.dart';
+// import 'package:flutter_driver/flutter_driver.dart';
+// import 'package:test/test.dart';
 
-void main() {
-  group('LOGIN TEST', () {
-    FlutterDriver driver;
-    String loginEmail, loginPassword;
+// void main() {
+//   group('LOGIN TEST', () {
+//     FlutterDriver driver;
+//     String loginEmail, loginPassword;
 
-    setUp(() async {
-      driver = await FlutterDriver.connect();
+//     setUp(() async {
+//       driver = await FlutterDriver.connect();
 
-      // read config file
-      //loginEmail = Config.testLoginEmail;
-      //loginPassword = Config.testLoginPassword;
-    });
+//       // read config file
+//       //loginEmail = Config.testLoginEmail;
+//       //loginPassword = Config.testLoginPassword;
+//     });
 
-    tearDown(() async {
-      if (driver != null) {
-        driver.close();
-      }
-    });
+//     tearDown(() async {
+//       if (driver != null) {
+//         driver.close();
+//       }
+//     });
 
-    test('No input provided by user test', () async {
-      await driver.tap(find.text('LOGIN'));
-      await driver.waitFor(find.text('Please enter your email'));
-      await driver.waitFor(find.text('Please enter your password'));
-    });
+//     test('No input provided by user test', () async {
+//       await driver.tap(find.text('LOGIN'));
+//       await driver.waitFor(find.text('Please enter your email'));
+//       await driver.waitFor(find.text('Please enter your password'));
+//     });
 
-    test('Details filled by user and login', () async {
-      await driver.tap(find.byValueKey(LoginKeys.emailKey));
-      await driver.enterText(loginEmail);
+//     test('Details filled by user and login', () async {
+//       await driver.tap(find.byValueKey(LoginKeys.emailKey));
+//       await driver.enterText(loginEmail);
 
-      await driver.tap(find.byValueKey(LoginKeys.passwordKey));
-      await driver.enterText(loginPassword);
+//       await driver.tap(find.byValueKey(LoginKeys.passwordKey));
+//       await driver.enterText(loginPassword);
 
-      await driver.tap(find.text('LOGIN'));
+//       await driver.tap(find.text('LOGIN'));
 
-      //await driver.waitFor(find.byType(''));
-    });
-  });
-}
+//       //await driver.waitFor(find.byType(''));
+//     });
+//   });
+// }
