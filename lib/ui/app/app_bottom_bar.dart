6d7ab79bd66a7f0b6013d68248c1c7fcac5a5b66
Rody Davis <rdavis@unifycrm.com>
2018-12-03 08:41:01 -0500

    Update app_bottom_bar.dart

--- lib/ui/app/app_bottom_bar.dart
+++ lib/ui/app/app_bottom_bar.dart
@@ -17,15 +17,34 @@ class AppBottomBar extends StatefulWidget {
 }
 
 class _AppBottomBarState extends State<AppBottomBar> {
-  PersistentBottomSheetController _sortController;
+  PersistentBottomSheetController _sortController,
+      _filterController,
+      _groupsController;
+
+  bool _sortTap, _filterTap, _groupTap;
+
+  @override
+  void initState() {
+    _sortTap = false;
+    _filterTap = false;
+    _groupTap = false;
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     final _showSortSheet = () {
       if (_sortController != null) {
         _sortController.close();
+        setState(() {
+          _sortTap = false;
+        });
         return;
       }
+
+      setState(() {
+        _sortTap = true;
+      });
 
       _sortController = Scaffold.of(context).showBottomSheet((context) {
         return StoreConnector<AppState, ListUIState>(
@@ -61,6 +80,115 @@ class _AppBottomBarState extends State<AppBottomBar> {
 
       _sortController.closed.whenComplete(() {
         _sortController = null;
+        setState(() {
+          _sortTap = false;
+        });
+      });
+    };
+
+    final _showFilterSheet = () {
+      if (_filterController != null) {
+        _filterController.close();
+        setState(() {
+          _filterTap = false;
+        });
+        return;
+      }
+
+      setState(() {
+        _filterTap = true;
+      });
+
+      _filterController = Scaffold.of(context).showBottomSheet((context) {
+        return StoreConnector<AppState, ListUIState>(
+          converter: (Store<AppState> store) =>
+              store.state.getListState(widget.entityType),
+          builder: (BuildContext context, listUIState) {
+            return Container(
+              color: Theme.of(context).backgroundColor,
+              child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: widget.sortFields.map((sortField) {
+                    return RadioListTile(
+                      dense: true,
+                      // TODO replace with localization
+                      title: Text(
+                          sortField[0].toUpperCase() + sortField.substring(1)),
+                      subtitle: sortField == listUIState.sortField
+                          ? Text(listUIState.sortAscending
+                              ? 'Ascending'
+                              : 'Descending')
+                          : null,
+                      groupValue: listUIState.sortField,
+                      onChanged: (value) {
+                        widget.onSelectedSortField(value);
+                      },
+                      value: sortField,
+                    );
+                  }).toList()),
+            );
+          },
+        );
+      });
+
+      _filterController.closed.whenComplete(() {
+        _filterController = null;
+        setState(() {
+          _filterTap = false;
+        });
+      });
+    };
+
+    final _showGroupSheet = () {
+      if (_groupsController != null) {
+        _groupsController.close();
+        setState(() {
+          _groupTap = false;
+        });
+        return;
+      }
+
+      setState(() {
+        _groupTap = true;
+      });
+
+      _groupsController = Scaffold.of(context).showBottomSheet((context) {
+        return StoreConnector<AppState, ListUIState>(
+          converter: (Store<AppState> store) =>
+              store.state.getListState(widget.entityType),
+          builder: (BuildContext context, listUIState) {
+            return Container(
+              color: Theme.of(context).backgroundColor,
+              child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: widget.sortFields.map((sortField) {
+                    return RadioListTile(
+                      dense: true,
+                      // TODO replace with localization
+                      title: Text(
+                          sortField[0].toUpperCase() + sortField.substring(1)),
+                      subtitle: sortField == listUIState.sortField
+                          ? Text(listUIState.sortAscending
+                              ? 'Ascending'
+                              : 'Descending')
+                          : null,
+                      groupValue: listUIState.sortField,
+                      onChanged: (value) {
+                        widget.onSelectedSortField(value);
+                      },
+                      value: sortField,
+                    );
+                  }).toList()),
+            );
+          },
+        );
+      });
+
+      _groupsController.closed.whenComplete(() {
+        _groupsController = null;
+        setState(() {
+          _groupTap = false;
+        });
       });
     };
 
@@ -71,17 +199,20 @@ class _AppBottomBarState extends State<AppBottomBar> {
           IconButton(
             tooltip: 'Sort',
             icon: Icon(Icons.sort_by_alpha),
-            onPressed: _showSortSheet,
+            onPressed:
+                _filterTap == true || _groupTap == true ? null : _showSortSheet,
           ),
           IconButton(
             tooltip: 'Filter',
             icon: Icon(Icons.filter_list),
-            onPressed: null,
+            onPressed:
+                _sortTap == true || _groupTap == true ? null : _showFilterSheet,
           ),
           IconButton(
             tooltip: 'Groups',
             icon: Icon(Icons.group),
-            onPressed: null,
+            onPressed:
+                _filterTap == true || _sortTap == true ? null : _showGroupSheet,
           ),
         ],
       ),
